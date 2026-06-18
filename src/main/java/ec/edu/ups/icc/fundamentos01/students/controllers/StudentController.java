b53c54cc4c752a2dc96edd9bd50a150fc75be84f
Mateo Paez <mpaez94144@gmail.com>
2026-06-18 10:59:39 -0500

    initial commit

---
+++ src/main/java/ec/edu/ups/icc/fundamentos01/students/controllers/StudentController.java
@@ -0,0 +1,34 @@
+package ec.edu.ups.icc.fundamentos01.students.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.ups.icc.fundamentos01.students.models.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>();
+
+    public StudentController() {
+        students.add(new Student(1L, "Juan", 30));
+        students.add(new Student(2L, "Diego", 10));
+    }
+
+    @GetMapping()
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @GetMapping("/count")
+    public String getCount() {
+        return "Total de estudiantes: " + students.size();
+    }
+    
+
+}
